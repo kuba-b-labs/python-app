261a2c3d629c4bdc5a7c9655dc8dbbd9c7e0bd02
Kuba123pl <119419376+Kuba123pl@users.noreply.github.com>
2025-07-04 16:13:35 +0200

    add api exception handling

--- app/routers/router.py
+++ app/routers/router.py
@@ -9,21 +9,30 @@ router = APIRouter()
 async def add_entry(request: Entry):
     """POST entry data to database"""
     async with postgresDB() as db:
-        await db.create_entry(request)
+        try:
+            await db.create_entry(request)
+        except Exception as error:
+            raise HTTPException(detail=f'Failed to add entry: {error}',status_code=500)
     return {"status" : "Entry successfully created"}
 
 @router.get("/entries")
 async def get_entries():
     """Get all database entries"""
     async with postgresDB() as db:
-        entries = await db.get_entries()
+        try:            
+            entries = await db.get_entries()
+        except Exception as error:
+            raise HTTPException(detail=f'Failed to get entries: {error}',status_code=500)
     return entries
 
 @router.get("/entry/{date}")
 async def get_entry(date : date):
     """Get database entry"""
     async with postgresDB() as db:
-        entries = await db.get_entries(date)
+        try:
+            entries = await db.get_entries(date)
+        except Exception as error:
+            raise HTTPException(detail=f'Failed to get all entries: {error}',status_code=500)
     return entries
 
 @router.put("/entry/")
@@ -33,7 +42,7 @@ async def update_entry(entry : Entry):
         try:
             await db.update_entry(entry)
         except Exception as error:
-            raise HTTPException(detail=f'Entry failed to update : {error}',status_code=500)
+            raise HTTPException(detail=f'Entry failed to update: {error}',status_code=500)
     return { "status" : "Entry updated successfully" }
 
 @router.delete("/entry/delete/{entry}")
