36dcf155126e20851e2a25db4c1df2e4ded8f47e
Kuba123pl <jakub.borowiec29@gmail.com>
2025-07-11 22:33:59 +0200

    test

--- terraform/modules/vnet/main.tf
+++ terraform/modules/vnet/main.tf
@@ -8,7 +8,7 @@ resource "azurerm_virtual_network" "vnet" {
 resource "azurerm_subnet" "subnet" {
   name                 = "internal"
   resource_group_name  = var.rg_name
-  virtual_network_name = var.subnet_name
+  virtual_network_name = var.vnet_name
   address_prefixes     = ["10.0.2.0/24"]
 }
 
